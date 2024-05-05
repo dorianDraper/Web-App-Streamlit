acfaf721a42c9b3c4a2dbfbad05a380bfbcb1fb8
Jorge Antonio Payà Albero <56591898+dorianDraper@users.noreply.github.com>
2024-05-05 09:00:02 +0000

    prep-spam-model1

--- src/app.py
+++ src/app.py
@@ -1,3 +1,24 @@
 import streamlit as st
+from pickle import load
 
-st.title('Hello World')
+model = load(open("/workspaces/Web-App-Streamlit/models/decision_tree_classifier_default_42.sav", "rb"))
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
+}
+
+st.title("Iris - Model prediction")
+
+val1 = st.slider("Petal width", min_value = 0.0, max_value = 4.0, step = 0.1)
+val2 = st.slider("Petal length", min_value = 0.0, max_value = 4.0, step = 0.1)
+val3 = st.slider("Sepal width", min_value = 0.0, max_value = 4.0, step = 0.1)
+val4 = st.slider("Sepal length", min_value = 0.0, max_value = 4.0, step = 0.1)
+
+if st.button("Predict"):
+    prediction = str(model.predict([[val1, val2, val3, val4]])[0])
+    pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
